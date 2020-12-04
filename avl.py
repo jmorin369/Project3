29161730d28c636af36012aa4dee364ae25f096a
Poliana Santana <poliana@Daniels-MacBook-Pro-3.local>
2020-12-04 02:40:29 -0500

    missing a few things, but getting there

--- avl.py
+++ avl.py
@@ -1,201 +1,201 @@
-# class Node:
-#     def __init__(self, word, count):
-#         self.count = count
-#         self.word  = word
-#         self.left  = None
-#         self.right = None
+class Node:
+    def __init__(self, word, count):
+        self.count = count
+        self.word  = word
+        self.left  = None
+        self.right = None
 
 
-# class AVL(object):
+class AVL(object):
     
-#     def __init__(self):
-#         self.nodes = 0
-#         self.root  = None
+    def __init__(self):
+        self.nodes = 0
+        self.root  = None
 
-# #########################################################
-#                ### Private Functions ###
-# #########################################################
+#########################################################
+               ### Private Functions ###
+#########################################################
 
-#     def __rotateLeft(self, root):
-#         if (not root):
-#             return root
-#         child  = root.right
-#         gchild = root.right.left
+    def __rotateLeft(self, root):
+        if (not root):
+            return root
+        child  = root.right
+        gchild = root.right.left
 
-#         child.left = root
-#         root.right = gchild
-#         return child
+        child.left = root
+        root.right = gchild
+        return child
 
-#     def __rotateRight(self, root):        
-#         if (not root):
-#             return root 
-#         child  = root.left
-#         gchild = root.left.right
+    def __rotateRight(self, root):        
+        if (not root):
+            return root 
+        child  = root.left
+        gchild = root.left.right
 
-#         root.left   = gchild
-#         child.right = root
-#         return child
+        root.left   = gchild
+        child.right = root
+        return child
 
-#     def __rotateLeftRight(self, root):
-#         if (not root):
-#             return root
-#         child   = root.left
-#         gchild  = root.left.right
-#         ggchild = root.left.right.left
+    def __rotateLeftRight(self, root):
+        if (not root):
+            return root
+        child   = root.left
+        gchild  = root.left.right
+        ggchild = root.left.right.left
 
-#         # Leftward Rotation
-#         gchild.left = child
-#         child.right = ggchild
-#         root.left   = gchild
+        # Leftward Rotation
+        gchild.left = child
+        child.right = ggchild
+        root.left   = gchild
 
-#         # Rightward Rotation
-#         return self.__rotateRight(root)
+        # Rightward Rotation
+        return self.__rotateRight(root)
 
-#     def __rotateRightLeft(self, root):
-#         if (not root):
-#                 return root
-#         child   = root.right;
-#         gchild  = root.right.left;
-#         ggchild = root.right.left.right;
+    def __rotateRightLeft(self, root):
+        if (not root):
+                return root
+        child   = root.right
+        gchild  = root.right.left
+        ggchild = root.right.left.right
 
-#         # Rightward Rotation
-#         gchild.right = child;
-#         child.left   = ggchild;
-#         root.right   = gchild;
+        # Rightward Rotation
+        gchild.right = child
+        child.left   = ggchild
+        root.right   = gchild
 
-#         # Leftward Rotation
-#         return self.__rotateLeft(root)
+        # Leftward Rotation
+        return self.__rotateLeft(root)
     
-#     def findHeight(self, root):
-#         height = 0;
-#         if (not root):
-#             return height
+    def findHeight(self, root):
+        height = 0
+        if (not root):
+            return height
         
-#         # Recursive Case
-#         height = max(height, self.findHeight(root.left))
-#         height = max(height, self.findHeight(root.right))
+        # Recursive Case
+        height = max(height, self.findHeight(root.left))
+        height = max(height, self.findHeight(root.right))
 
-#         # Base Case
-#         return height + 1
+        # Base Case
+        return height + 1
 
-# #########################################################
-#                 ### Public Functions ###
-# #########################################################
+#########################################################
+                ### Public Functions ###
+#########################################################
     
-#     def insert(self, root, word):
-#         # BST Search
-#         if (not root):
-#             self.nodes += 1
-#             return Node(word, 1)
-#         elif (root.word > word):
-#             root.left  = self.insert(root.left, word)
-#         elif (root.word < word):
-#             root.right = self.insert(root.right, word)
-#         else:
-#             root.count += 1
-#             return root
+    def insert(self, root, word):
+        # BST Search
+        if (not root):
+            self.nodes += 1
+            return Node(word, 1)
+        elif (root.word > word):
+            root.left  = self.insert(root.left, word)
+        elif (root.word < word):
+            root.right = self.insert(root.right, word)
+        else:
+            root.count += 1
+            return root
                     
-#         # Balance tree
-#         bf = self.findHeight(root.left) - self.findHeight(root.right)
-#         if (bf > 1):
-# #             child_bf = self.findHeight(root.left.left) - self.findHeight(root.left.right)
-#             if (word < root.left.word): 
-# #                 print("{0} Right".format(root.worpd))
-#                 return self.__rotateRight(root)
-#             else:
-# #                 print("{0} LeftRight".format(root.word))
-#                 return self.__rotateLeftRight(root)
-#         elif (bf < -1):
-# #             child_bf = self.findHeight(root.right.left) - self.findHeight(root.right.right)
-#             if (word < root.right.word):
-# #                 print("{0} RightLeft".format(root.word))
-#                 return self.__rotateRightLeft(root)
-#             else:
-# #                 print("{0} Left".format(root.word))
-#                 return self.__rotateLeft(root)
+        # Balance tree
+        bf = self.findHeight(root.left) - self.findHeight(root.right)
+        if (bf > 1):
+#             child_bf = self.findHeight(root.left.left) - self.findHeight(root.left.right)
+            if (word < root.left.word): 
+#                 print("{0} Right".format(root.worpd))
+                return self.__rotateRight(root)
+            else:
+#                 print("{0} LeftRight".format(root.word))
+                return self.__rotateLeftRight(root)
+        elif (bf < -1):
+#             child_bf = self.findHeight(root.right.left) - self.findHeight(root.right.right)
+            if (word < root.right.word):
+#                 print("{0} RightLeft".format(root.word))
+                return self.__rotateRightLeft(root)
+            else:
+#                 print("{0} Left".format(root.word))
+                return self.__rotateLeft(root)
 
-# # key < node->left->key
+# key < node->left->key
                 
-#         return root
+        return root
     
-#     def add(self, word):
-#         if (not self.root):
-#             self.root = Node(word,1)
-#         else:
-#             self.root = self.insert(self.root, word)
+    def add(self, word):
+        if (not self.root):
+            self.root = Node(word,1)
+        else:
+            self.root = self.insert(self.root, word)
             
-#     def printPreOrder(self, root):
-#         if (not root):
-#             return
-#         print("{0}: {1}".format(root.word, root.count))
-#         self.printPreOrder(root.left)
-#         self.printPreOrder(root.right)   
+    def printPreOrder(self, root):
+        if (not root):
+            return
+        print("{0}: {1}".format(root.word, root.count))
+        self.printPreOrder(root.left)
+        self.printPreOrder(root.right)   
             
-#     def printInOrder(self, root):
-#         if (not root):
-#             return
-#         self.printInOrder(root.left)
-#         print("{0}: {1}".format(root.word, root.count))
-#         self.printInOrder(root.right)
+    def printInOrder(self, root):
+        if (not root):
+            return
+        self.printInOrder(root.left)
+        print("{0}: {1}".format(root.word, root.count))
+        self.printInOrder(root.right)
         
-#     def printPostOrder(self, root):
-#         if (not root):
-#             return
-#         self.printPostOrder(root.left)
-#         self.printPostOrder(root.right)
-#         print("{0}: {1}".format(root.word, root.count))
+    def printPostOrder(self, root):
+        if (not root):
+            return
+        self.printPostOrder(root.left)
+        self.printPostOrder(root.right)
+        print("{0}: {1}".format(root.word, root.count))
         
-#     def printTree(self):
-#         self.printInOrder(self.root)
-# #         print()
-# #         self.printPreOrder(self.root)
-# #         print()
-# #         self.printPostOrder(self.root)
+    def printTree(self):
+        self.printInOrder(self.root)
+#         print()
+#         self.printPreOrder(self.root)
+#         print()
+#         self.printPostOrder(self.root)
         
         
-#     def printCount(self):
-#         print(self.nodes)
+    def printCount(self):
+        print(self.nodes)
         
-#     def printHeight(self):
-#         print(self.findHeight(self.root))
+    def printHeight(self):
+        print(self.findHeight(self.root))
 
 
-# from pathlib import Path as path
-# speeches = {"BattleCreekDec19_2019",
-#             "BemidjiSep18_2020", 
-#             "CharlestonFeb28_2020",
-#             "CharlotteMar2_2020",
-#             "CincinnatiAug1_2019",
-#             "ColoradorSpringsFeb20_2020",
-#             "DallasOct17_2019",
-#             "DesMoinesJan30_2020",
-#             "FayettevilleSep9_2019",
-#             "FayettevilleSep19_2020",
-#             "FreelandSep10_2020",
-#             "GreenvilleJul17_2019",
-#             "HendersonSep13_2020",
-#             "HersheyDec10_2019",
-#             "LasVegasFeb21_2020",
-#             "LatrobeSep3_2020",
-#             "LexingtonNov4_2019",
-#             "MilwaukeeJan14_2020",
-#             "MindenSep12_2020",
-#             "MinneapolisOct10_2019",
-#             "MosineeSep17_2020",
-#             "NewHampshireAug15_2019",
-#             "NewHampshireAug28_2020",
-#             "NewHampshireFeb10_2020",
-#             "NewMexicoSep16_2019",
-#             "OhioSep21_2020",
-#             "PhoenixFeb19_2020",
-#             "PittsburghSep22_2020",
-#             "TexasSep23_2019",
-#             "ToledoJan9_2020",
-#             "TulsaJun20_2020",
-#             "TupeloNov1_2019",
-#             "WildwoodJan28_2020",
-#             "Winston-SalemSep8_2020",
-#             "YumaAug18_2020"}
+from pathlib import Path as path
+speeches = {"BattleCreekDec19_2019",
+            "BemidjiSep18_2020", 
+            "CharlestonFeb28_2020",
+            "CharlotteMar2_2020",
+            "CincinnatiAug1_2019",
+            "ColoradorSpringsFeb20_2020",
+            "DallasOct17_2019",
+            "DesMoinesJan30_2020",
+            "FayettevilleSep9_2019",
+            "FayettevilleSep19_2020",
+            "FreelandSep10_2020",
+            "GreenvilleJul17_2019",
+            "HendersonSep13_2020",
+            "HersheyDec10_2019",
+            "LasVegasFeb21_2020",
+            "LatrobeSep3_2020",
+            "LexingtonNov4_2019",
+            "MilwaukeeJan14_2020",
+            "MindenSep12_2020",
+            "MinneapolisOct10_2019",
+            "MosineeSep17_2020",
+            "NewHampshireAug15_2019",
+            "NewHampshireAug28_2020",
+            "NewHampshireFeb10_2020",
+            "NewMexicoSep16_2019",
+            "OhioSep21_2020",
+            "PhoenixFeb19_2020",
+            "PittsburghSep22_2020",
+            "TexasSep23_2019",
+            "ToledoJan9_2020",
+            "TulsaJun20_2020",
+            "TupeloNov1_2019",
+            "WildwoodJan28_2020",
+            "Winston-SalemSep8_2020",
+            "YumaAug18_2020"}
 
 # def input_command():
 #     "Takes in various user commands"
@@ -213,19 +213,17 @@
 #         speech = input()
 #     return speech
 
-# # Initialize Structures
+# Initialize Structures
 # omap = AVL()
 
 # # Take in and format speech file
-# speech = input_command();
-# file_path = path(".\\Speeches\\" + speech + ".txt")
+# speech = input_command()
+# file_path = path("./Speeches/" + speech + ".txt")
 
-# # Determine file format
-# if file_path.is_file():
-#     file = open(file_path,"r")
-    
+# def parse(text):
 #     word = ""
-#     for line in file: 
+#     # file = open(file)
+#     for line in text: 
 #         line = line.lower()
 #         for char in line:
 #             if char == " " or char == "\"" or char == "," or char == "." or char == "?" or char == "…" \
@@ -236,6 +234,12 @@
 #                 word = ""
 #             else:
 #                 word += char
+
+# # Determine file format
+# if file_path.is_file():
+#     file = open(file_path,"r")
+    
+#     parse(file)
         
 #     file.close()
 #     omap.printTree()
@@ -243,22 +247,12 @@
 # elif speech == 'ALL' or speech == '"ALL"':
 #     # Parse all inputs
 #     for speech in speeches:
-#         file_path = path(".\\Speeches\\" + speech + ".txt")
+#         file_path = path("./Speeches/" + speech + ".txt")
 #         print("Reached: %s" % speech)
 #         file = open(file_path,"r")
 
-#         word = ""
-#         for line in file: 
-#             line = line.lower()
-#             for char in line:
-#                 if char == " " or char == "\"" or char == "," or char == "." or char == "?" or char == "…" \
-#                     or char == "€" or char == "¦" or char >= "Ç":
-#                     if word != "":
-# #                         print(word)
-#                         omap.add(word)
-#                     word = ""
-#                 else:
-#                     word += char
+#         parse(file)
+
 #         omap.printCount()
 #         omap.printHeight()
 #         file.close()
--- gui.py
+++ gui.py
@@ -1,14 +1,55 @@
+import os
 import tkinter as tk
+import hashtb as ht
+import avl as tree
+
+currdir = os.path.dirname(__file__)
+
+# avl tree & hash table nteraction
+omap = tree.AVL()
+obj = ht.HashTable()
+
+spch = [] 
+for item in tree.speeches:
+    spch.append(item) # TODO: make this be in order??
+
+def parse(text):
+    word = ""
+    # file = open(file)
+    for line in text: 
+        line = line.lower()
+        for char in line: 
+            if char == " " or char == "\"" or char == "," or char == "." or char == "?" or char == "…" \
+                or char == "€" or char == "¦" or char >= "Ç":
+                if word != "":
+                    # TODO: time these functions
+                    omap.add(word)
+                    obj.insert(word)
+                word = ""
+            else:
+                word += char
+
+
+def makeFile(rallyList):
+    text = ''
+    for it in range(len(rallyList)):
+        file = rallyList[it] + '.txt'
+        with open(file) as f:
+            text += f.read()
+            # print(file)
+    return text
+
 
 h = 600
 w = 800
-
 root = tk.Tk()
 canvas = tk.Canvas(root, height=h, width=w)
 canvas.pack()
 
-
 # cloud
+
+# TODO: make sure cloud is retrieving proper text file
+
 wcPic = tk.PhotoImage(file='wc.png')
 trump = tk.PhotoImage(file='trump3.png')
 
@@ -43,33 +84,47 @@ searchButton = tk.Button(searchFrame, text="search", command=lambda: printInfo(s
 searchButton.place(relx=0.5, relwidth=0.5, relheight=0.5)
 
 def printInfo(word):
-    tempstr = ''
+    rallyList = []
+    rallyStr = ''
     selectedRallies = box.curselection()
     for i in range(len(selectedRallies)):
-        tempstr += str(selectedRallies[i]) + ', '
-    tempstr = 'From rallies ' + tempstr
-    infostr = 'You searched for: %s \n%s\n%s was said x times \n' % (word, tempstr, word)
-    linfo['text'] = infostr
+        rallyList.append(box.get(selectedRallies[i]))
+        rallyStr += box.get(selectedRallies[i]) + '\n'
+    # rallyList is a list of selected rally/file names to send to parsing function
+    # TODO: call makeFile(rallyList), which will call parse(newfile) to collect appropriate data
+    #       there should also be a way to retrieve the time it took to make & search each data structure
+    #       this time must outputted into the info string below
+    #           TODO: actually maybe move this to a different button
+    parse( makeFile(rallyList) )
+    # TODO: call count(word) to retrieve count and pass it to the string below
+    infostr = 'You searched for: %s \n' % (word)
+    infostr += 'From Rallies: \n%s' % (rallyStr)
+    cntHash = obj.getCount(word)
+    cntTree = 'AVL search function???'
+    infostr += 'Results from:\n'
+    infostr += 'Hash Table: \t\t\t AVL Tree:\n'
+    infostr += 'Count: %d \t\t\t //TODO: %s' % (cntHash, cntTree)
+    infostr += '//TODO: Print Search & Insert Time'
+    infoLabel['text'] = infostr
 
 # rally selection
 select = tk.Frame(root, bg="black")
 select.place(relx=0.05, rely=0.2, relwidth=0.2, relheight=0.7)
 
-box = tk.Listbox(select, bg="white", selectmode=tk.EXTENDED)
-for x in range(31):
-    if x==0:
-        box.insert(x, "ALL")
-    else:
-        box.insert(x, str(x))
+box = tk.Listbox(select, bg="white", selectmode=tk.BROWSE)
+for x in range(len(spch)):
+    box.insert(x, spch[x])
 box.place(relx=0, rely=0, relwidth=1, relheight=1)
-# call function with appropriate file names
+# TODO: make parse button to press upon selection
+#       instruct user to parse first, then search
+# TODO LAST: make sure multiple selection works?
 
 
 # display info
-info = tk.Frame(root, bg="black")
-info.place(relx=0.30, rely=0.05, relwidth=0.6, relheight=0.3)
+infoFrame = tk.Frame(root, bg="black")
+infoFrame.place(relx=0.30, rely=0.05, relwidth=0.6, relheight=0.3)
 
-linfo = tk.Label(info, text="display info about word")
-linfo.place(relwidth=1, relheight=1)
+infoLabel = tk.Label(infoFrame, text="display info about word")
+infoLabel.place(relwidth=1, relheight=1)
 
 root.mainloop()
